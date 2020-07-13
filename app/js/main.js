f08f647250152b479362c778037cd4059262370f
Sihula <serhiisihula@gmail.com>
2020-07-13 11:49:52 +0200

    added slider feature

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,10 @@
-$(function({
-
-
-    
-}));
+$(function () {
+  $(".slider-top").slick({
+    nextArrow:
+    '<button class="slick-arrow slick-next" ><img src="images/icons/next.png" alt="next arrow"></button>',
+  prevArrow:
+    '<button class="slick-arrow slick-prev" ><img src="images/icons/prev.png" alt="prev arrow"></button>',
+    infinite: true,
+    slidesToShow: 1,
+  });
+});
