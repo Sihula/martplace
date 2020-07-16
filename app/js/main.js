c5bb71a7bdc9cc342948007e129d57a17d7b288e
Sihula <serhiisihula@gmail.com>
2020-07-16 16:23:15 +0200

    added shooping section

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,21 @@
 $(function () {
   $(".slider-top").slick({
     nextArrow:
-    '<button class="slick-arrow slick-next" ><img src="images/icons/next.png" alt="next arrow"></button>',
-  prevArrow:
-    '<button class="slick-arrow slick-prev" ><img src="images/icons/prev.png" alt="prev arrow"></button>',
+      '<button class="slick-arrow slick-next" ><img src="images/icons/next.png" alt="next arrow"></button>',
+    prevArrow:
+      '<button class="slick-arrow slick-prev" ><img src="images/icons/prev.png" alt="prev arrow"></button>',
     infinite: true,
     slidesToShow: 1,
   });
+
+  $(".slier-mix__box").slick({
+    nextArrow:
+      '<button class="slick-arrow slick-next" ><img src="images/icons/next.png" alt="next arrow"></button>',
+    prevArrow:
+      '<button class="slick-arrow slick-prev" ><img src="images/icons/prev.png" alt="prev arrow"></button>',
+    infinite: true,
+    slidesToShow: 3,
+  });
+
+  var mixer = mixitup(".products__inner-box");
 });
